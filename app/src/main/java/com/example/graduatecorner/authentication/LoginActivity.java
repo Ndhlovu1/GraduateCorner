f7104bb4747aa9ccda17ef6122ff9e72b2f72b6f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-05-16 17:30:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/graduatecorner/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduatecorner.authentication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.graduatecorner.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
